09c3fe3e673256650190bbb61dc3f4afd4532a32
Krish <111554369+krishanprajapat23@users.noreply.github.com>
2025-12-16 16:59:35 +0530

    Refactor Header component for improved structure and readability

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -31,12 +31,9 @@ const Header = () => {
                 opacity: isSticky ? 1 : 1,
             }}
             transition={{ type: "spring", stiffness: 300, damping: 30 }}
-            className={`
-            text-(--primary-t-color)} w-full z-99 text-(--primary-t-color)
-            ${
-            isSticky ? "fixed top-0 bg-[hsl(60deg_33.33%_1.18%/42%)] backdrop-blur-md shadow-lg" : "relative bg-transparent pt-5"
-            }        
-            `}>
+            className={`text-(--primary-t-color)} w-full z-99 text-(--primary-t-color) 
+                ${isSticky ? "fixed top-0 bg-[hsl(60deg_33.33%_1.18%/42%)] backdrop-blur-md shadow-lg" : "relative bg-transparent pt-5"
+            }`}>
                 <div className="container mx-auto px-4">
                     <nav className="flex items-center justify-between h-20">
 
@@ -81,39 +78,39 @@ const Header = () => {
                 </div>
 
                 {/* Mobile Sidebar */}
-                <Activity mode={isShowingSidebar ? "visible" : "hidden"}>
-                    <aside className="bg-(--dark-grey-color) fixed right-0 top-0 min-h-screen w-64 p-6 z-50">
+                    <Activity mode={isShowingSidebar ? "visible" : "hidden"}>
+                        <aside className="bg-(--dark-grey-color) fixed right-0 top-0 min-h-screen w-64 p-6 z-50">
 
-                        {/* Close Button */}
-                        <button
-                            className="btn mb-5 text-(--white-color) bg-(--danger-color) rounded-xs text-sm p-2"
-                            onClick={() => setIsShowingSidebar(false)}
-                        >
-                            {/* X Icon */}
-                            <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5" fill="none"
-                                viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-                                <path strokeLinecap="round" strokeLinejoin="round"
-                                    d="M6 18L18 6M6 6l12 12" />
-                            </svg>
-                        </button>
+                            {/* Close Button */}
+                            <button
+                                className="btn mb-5 text-(--white-color) bg-(--danger-color) rounded-xs text-sm p-2"
+                                onClick={() => setIsShowingSidebar(false)}
+                            >
+                                {/* X Icon */}
+                                <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5" fill="none"
+                                    viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                                    <path strokeLinecap="round" strokeLinejoin="round"
+                                        d="M6 18L18 6M6 6l12 12" />
+                                </svg>
+                            </button>
 
-                        {/* Sidebar Nav */}
-                        <nav className="flex flex-col gap-2 text-lg my-4">
-                            {
-                                NAV_ITEMS.map((item) => (
-                                    <a
-                                        onClick={() => setIsShowingSidebar(false)}
-                                        href={item.href}
-                                        key={item.id}
-                                        className="link-underline text-(--white-color) p-2 font-semibold hover:text-(--primary-color) transition-colors"
-                                    >
-                                        {item.label}
-                                    </a>
-                                ))
-                            }
-                        </nav>
-                    </aside>
-                </Activity>
+                            {/* Sidebar Nav */}
+                            <nav className="flex flex-col gap-2 text-lg my-4">
+                                {
+                                    NAV_ITEMS.map((item) => (
+                                        <a
+                                            onClick={() => setIsShowingSidebar(false)}
+                                            href={item.href}
+                                            key={item.id}
+                                            className="link-underline text-(--white-color) p-2 font-semibold hover:text-(--primary-color) transition-colors"
+                                        >
+                                            {item.label}
+                                        </a>
+                                    ))
+                                }
+                            </nav>
+                        </aside>
+                    </Activity>
             </motion.header>        
         </>
     );
